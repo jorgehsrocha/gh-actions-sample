80c25dcb418fe5240dd4d89a39f5d46164b37310
Jorge Rocha <jhrocha.sousa@gmail.com>
2021-03-14 11:58:41 -0300

    ci: 🎡 Implement Jest

---
+++ src/tests/calc.test.js
@@ -0,0 +1,13 @@
+import { Calc } from '../index.js';
+
+const calc = new Calc();
+
+test("Soma deve retornar 2", () => {
+    const result = calc.sum(1,1);
+    expect(result).toEqual(2);
+});
+
+test("Multiplicação deve retornar 6", () => {
+    const result = calc.multiply(3,2);
+    expect(result).toEqual(6);
+});
