b4162d90779a5a38472e83f843de4597f8813e44
Jorge Henrique <jhrocha.sousa@gmail.com>
2021-03-15 13:42:18 -0300

    Fixing the test

--- src/tests/calc.test.js
+++ src/tests/calc.test.js
@@ -8,6 +8,6 @@ test("Soma deve retornar 2", () => {
 });
 
 test("Multiplicação deve retornar 6", () => {
-    const result = calc.multiply(3,1);
+    const result = calc.multiply(3,2);
     expect(result).toEqual(6);
 });
